7426f4d6402e23bae50c088a08b7545ef42a4ff1
Sebastian Rubio <srubio604@gmail.com>
2025-11-26 12:27:33 -0300

    agregando contenidos segunda clase

---
+++ segunda_clase/contenido.js
@@ -0,0 +1,14 @@
+//PCA
+//1.-Estadistica
+//  -distribuciones
+//  -analisis de variable continuas y discretas
+//  -analisis bivariado
+//       -correlacion
+//       -regresion linear (indice de determinacion R2)
+//       -test estadistico
+//             -t de student
+//  -analisis multivariado
+//       - matrices de covarianza
+//       -autovectores y autovalores
+//       -componente principal
+//       -reduccion de dimensionalidad
